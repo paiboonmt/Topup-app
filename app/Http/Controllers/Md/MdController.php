9cd042f6e3962a431eddc21eb2ec1aa1fe1303a7
paiboonmt <paiboon500@gmail.com>
2024-11-02 09:14:49 +0700

    update

--- app/Http/Controllers/Md/MdController.php
+++ app/Http/Controllers/Md/MdController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\Md;
 
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Js;
 
 class MdController extends Controller
 {
